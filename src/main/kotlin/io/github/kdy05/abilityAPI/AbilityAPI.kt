935935f3d09ac7edccd5841ef50d20661e3ee1a2
Ararab <70100282+KDY05@users.noreply.github.com>
2026-06-22 08:11:51 +0900

    plugin 인스턴스 노출

--- src/main/kotlin/io/github/kdy05/abilityAPI/AbilityAPI.kt
+++ src/main/kotlin/io/github/kdy05/abilityAPI/AbilityAPI.kt
@@ -1,13 +1,20 @@
 package io.github.kdy05.abilityAPI
 
+import org.bukkit.plugin.Plugin
+
 object AbilityAPI {
     private var _service: AbilityService? = null
+    private var _plugin: Plugin? = null
 
     val service: AbilityService
-        get() = _service ?: error("AbilityAPI is not Initialized yet.")
+        get() = _service ?: error("AbilityAPI is not initialized yet.")
 
-    fun setup(service: AbilityService) {
+    val plugin: Plugin
+        get() = _plugin ?: error("AbilityAPI is not initialized yet.")
+
+    fun setup(service: AbilityService, plugin: Plugin) {
         if (_service != null) error("AbilityAPI is already initialized.")
         _service = service
+        _plugin = plugin
     }
 }
